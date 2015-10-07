8fafa686279b626d00950c86685a0b9c99bf2a5e
Ankit <amurark@ncsu.edu>
2015-10-07 03:44:15 -0400

    bug fixes

--- oodProject/libManager/db/migrate/20151007050045_default_status_in_books.rb
+++ oodProject/libManager/db/migrate/20151007050045_default_status_in_books.rb
@@ -1,5 +1,5 @@
 class DefaultStatusInBooks < ActiveRecord::Migration
   def change
-    change_column :books, :Status, :boolean, :null => false, :default => 0
+    change_column :books, :Status, :boolean, :null => false, :default => false
   end
 end
